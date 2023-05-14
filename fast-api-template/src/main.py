560dac56673074bdf34d72710c367d5b9e15e3b3
Harry Sadler <harrysadlermusic@gmail.com>
2023-05-14 20:48:39 +0200

    add some logging examples

--- fast-api-template/src/main.py
+++ fast-api-template/src/main.py
@@ -1,5 +1,10 @@
+import logging
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -7,6 +12,7 @@ app = FastAPI()
 # http GET http://localhost:8000/status/
 @app.get("/status/")
 async def status():
+    logger.info("Request to /status/")
     return "ok"
 
 
@@ -18,5 +24,6 @@ class Item(BaseModel):
 # http POST http://localhost:8000/items/ name=apple price:=1.23
 @app.post("/items/")
 async def create_item(item: Item):
+    logger.info("Request to /items/")
     item_dict = item.dict()
     return item_dict
