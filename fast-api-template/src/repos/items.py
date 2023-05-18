3433e0dd5bc69c18818665a6eaeef9c4d18cf2ca
Harry Sadler <harrysadlermusic@gmail.com>
2023-05-19 01:18:02 +0200

    put CRUD functions for input resource in repos

---
+++ fast-api-template/src/repos/items.py
@@ -0,0 +1,61 @@
+import logging
+from typing import Union
+
+import asyncpg
+
+from src.database import Database
+from src.models import Item, ItemIn
+
+logger = logging.getLogger(__name__)
+
+
+async def fetch_item_by_id(db: Database, item_id: int) -> Union[Item, None]:
+    FETCH_ITEM_BY_ID_COMMAND = """
+        SELECT * FROM item
+        WHERE id = $1
+    """
+
+    async with db.pool.acquire() as con:
+        item_record = await con.fetchrow(FETCH_ITEM_BY_ID_COMMAND, item_id)
+
+    return Item(**item_record) if item_record is not None else None
+
+
+async def fetch_items_by_ids(db: Database, item_ids: list[int]) -> list[Item]:
+    FETCH_ITEMS_BY_IDS_COMMAND = """
+        SELECT * FROM item
+        WHERE id = ANY($1::int[])
+    """
+
+    async with db.pool.acquire() as con:
+        fetched_records = await con.fetch(FETCH_ITEMS_BY_IDS_COMMAND, item_ids)
+
+    return [Item(**r) for r in fetched_records]
+
+
+async def create_item(db: Database, input_item: ItemIn) -> Item:
+    INSERT_ITEM_COMMAND = """
+        INSERT INTO item (name, price)
+        VALUES ($1, $2)
+        RETURNING id;
+    """
+
+    FETCH_ITEM_BY_ID_COMMAND = """
+        SELECT * FROM item
+        WHERE id = $1
+    """
+
+    try:
+        async with db.pool.acquire() as con:
+            item_id = await con.fetchval(INSERT_ITEM_COMMAND, input_item.name, input_item.price)
+            logger.debug("Item record inserted", extra={"item_id": item_id})
+            item_created_record = await con.fetchrow(FETCH_ITEM_BY_ID_COMMAND, item_id)
+
+        return Item(**item_created_record)
+
+    except asyncpg.exceptions.UniqueViolationError as e:
+        logger.debug(
+            "Item record could not be created because it violated a unique constraint",
+            extra={"error": e},
+        )
+        raise e
--- fast-api-template/src/routers/items.py
+++ fast-api-template/src/routers/items.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException, Path, Query
 
 from src import models
 from src.database import Database, get_database
+from src.repos import items as items_repo
 
 logger = logging.getLogger(__name__)
 
@@ -26,19 +27,9 @@ async def get_item(
     item_id: int = Path(gt=0, example=1), db: Database = Depends(get_database)
 ) -> models.ItemOutput_GET:
     logger.info("Fetching item by id", extra={"item_id": item_id})
-
-    FETCH_ITEM_BY_ID_COMMAND = """
-        SELECT * FROM item
-        WHERE id = $1
-    """
-
-    async with db.pool.acquire() as con:
-        item_record = await con.fetchrow(FETCH_ITEM_BY_ID_COMMAND, item_id)
-
-    if item_record is None:
+    item = await items_repo.fetch_item_by_id(db, item_id)
+    if item is None:
         raise HTTPException(status_code=404, detail="Item resource not found")
-
-    item = models.Item(**item_record)
     logger.info("Item fetched", extra={"item": dict(item)})
     return models.ItemOutput_GET(item=item)
 
@@ -48,16 +39,7 @@ async def get_items(
     item_ids: list[int] = Query(gt=0, example=[1, 2]), db: Database = Depends(get_database)
 ) -> models.ItemsOutput_GET:
     logger.info("Fetching items by ids", extra={"item_ids": item_ids})
-
-    FETCH_ITEMS_BY_IDS_COMMAND = """
-        SELECT * FROM item
-        WHERE id = ANY($1::int[])
-    """
-
-    async with db.pool.acquire() as con:
-        fetched_records = await con.fetch(FETCH_ITEMS_BY_IDS_COMMAND, item_ids)
-
-    items = [models.Item(**r) for r in fetched_records]
+    items = await items_repo.fetch_items_by_ids(db, item_ids)
     logger.info("Items fetched", extra={"items": [dict(item) for item in items]})
     return models.ItemsOutput_GET(items=items)
 
@@ -73,32 +55,12 @@ async def get_items(
 async def create_item(
     input: models.ItemInput_POST, db: Database = Depends(get_database)
 ) -> models.ItemOutput_POST:
-    logger.info("Inserting item", extra={"item": input.item})
-
-    INSERT_ITEM_COMMAND = """
-        INSERT INTO item (name, price)
-        VALUES ($1, $2)
-        RETURNING id;
-    """
-
-    FETCH_ITEM_BY_ID_COMMAND = """
-        SELECT * FROM item
-        WHERE id = $1
-    """
+    logger.info("Inserting item", extra={"item": dict(input.item)})
 
     try:
-        async with db.pool.acquire() as con:
-            item_id = await con.fetchval(INSERT_ITEM_COMMAND, input.item.name, input.item.price)
-            logger.debug("Item record inserted", extra={"item_id": item_id})
-            item_created_record = await con.fetchrow(FETCH_ITEM_BY_ID_COMMAND, item_id)
-
-        item = models.Item(**item_created_record)
-        logger.info("Created item", extra={"item": dict(item)})
-        return models.ItemOutput_POST(item_created=item)
-
-    except asyncpg.exceptions.UniqueViolationError as e:
-        logger.debug(
-            "Item record could not be created because it violated a unique constraint",
-            extra={"error": e},
-        )
+        item_created = await items_repo.create_item(db, input.item)
+    except asyncpg.exceptions.UniqueViolationError:
         raise HTTPException(status_code=409, detail="Resource already exists")
+
+    logger.info("Created item", extra={"item": dict(item_created)})
+    return models.ItemOutput_POST(item_created=item_created)
