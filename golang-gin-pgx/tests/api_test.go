6bdfefae48e3bae7d793b7a95456cafc699d43d5
hsadler <harrysadlermusic@gmail.com>
2023-11-09 22:27:47 +0100

    get a couple go project tests working

--- golang-gin-pgx/tests/api_test.go
+++ golang-gin-pgx/tests/api_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -24,13 +25,10 @@ func performRequest(r http.Handler, method, path string) *httptest.ResponseRecor
 func TestStatus(t *testing.T) {
 	r := gin.Default()
 	r.GET("/status", routes.HandleStatus)
-
 	w := performRequest(r, "GET", "/status")
-
 	if w.Code != http.StatusOK {
 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, w.Code)
 	}
-
 	expected := `{"status":"ok"}`
 	if w.Body.String() != expected {
 		t.Errorf("Expected %s, but got %s", expected, w.Body.String())
@@ -40,13 +38,10 @@ func TestStatus(t *testing.T) {
 func TestMetrics(t *testing.T) {
 	r := gin.Default()
 	r.GET("/metrics", routes.HandleMetrics(r))
-
 	w := performRequest(r, "GET", "/metrics")
-
 	if w.Code != http.StatusOK {
 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, w.Code)
 	}
-
 	expectedSubstring := "go_"
 	if !strings.Contains(w.Body.String(), expectedSubstring) {
 		t.Errorf("Expected response body to contain substring %s, but got %s", expectedSubstring, w.Body.String())
@@ -65,53 +60,82 @@ func TestGetAllItems(t *testing.T) {
 	)
 	defer deps.CleanupDependencies()
 	// define mock DB expectations
-	mockDBPool.ExpectQuery("SELECT id, uuid, created_at, name, price FROM item")
+	rows := mockDBPool.NewRows([]string{"id", "uuid", "created_at", "name", "price"}).
+		AddRow(
+			1,
+			"550e8400-e29b-41d4-a716-446655440000",
+			time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC),
+			"pi",
+			float32(3.14),
+		).
+		AddRow(
+			2,
+			"550e8400-e29b-41d4-a716-446655440001",
+			time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC),
+			"tree-fiddy",
+			float32(3.50),
+		)
+	mockDBPool.ExpectQuery("SELECT (.+) FROM item").
+		WillReturnRows(rows)
 	// setup router
 	r := gin.Default()
 	r.GET("/api/items/all", routes.HandleGetAllItems(deps))
-	// make request
+	// exec request
 	w := performRequest(r, "GET", "/api/items/all")
-	t.Logf("Status: %d", w.Code)
-	t.Logf("Response: %s", w.Body.String())
 	// assert response code
 	if w.Code != http.StatusOK {
 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, w.Code)
 	}
-	// assert response body
-	expectedSubstring := `"id":1`
-	if !strings.Contains(w.Body.String(), expectedSubstring) {
-		t.Errorf("Expected response body to contain substring %s, but got %s", expectedSubstring, w.Body.String())
+	// assert full response body
+	expected := `{"data":[{"id":1,"uuid":"550e8400-e29b-41d4-a716-446655440000","created_at":"2021-01-01T00:00:00Z","name":"pi","price":3.14},{"id":2,"uuid":"550e8400-e29b-41d4-a716-446655440001","created_at":"2021-01-01T00:00:00Z","name":"tree-fiddy","price":3.5}],"meta":{}}`
+	if w.Body.String() != expected {
+		t.Errorf("Expected %s, but got %s", expected, w.Body.String())
+	}
+	// assert db expectations were met
+	if err := mockDBPool.ExpectationsWereMet(); err != nil {
+		t.Errorf("There were unfulfilled DB expectations: %s", err)
 	}
 }
 
-// func TestGetItem(t *testing.T) {
-// 	// setup mock dependencies
-// 	mockDBPool, err := database.SetupTestDB()
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	deps := dependencies.NewDependencies(
-// 		validator.New(),
-// 		mockDBPool,
-// 	)
-// 	defer deps.CleanupDependencies()
-// 	// define mock DB expectations
-// 	mockDBPool.ExpectQuery("SELECT (.+) FROM item WHERE id = (.+)").
-// 		WithArgs(1)
-// 	// setup router
-// 	r := gin.Default()
-// 	r.GET("/api/items/:id", routes.HandleGetItem(deps))
-// 	// make request
-// 	w := performRequest(r, "GET", "/api/items/1")
-// 	t.Logf("Status: %d", w.Code)
-// 	t.Logf("Response: %s", w.Body.String())
-// 	// assert response code
-// 	if w.Code != http.StatusOK {
-// 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, w.Code)
-// 	}
-// 	// assert response body
-// 	expectedSubstring := `"id":1`
-// 	if !strings.Contains(w.Body.String(), expectedSubstring) {
-// 		t.Errorf("Expected response body to contain substring %s, but got %s", expectedSubstring, w.Body.String())
-// 	}
-// }
+func TestGetItem(t *testing.T) {
+	// setup mock dependencies
+	mockDBPool, err := pgxmock.NewPool()
+	if err != nil {
+		t.Fatal(err)
+	}
+	deps := dependencies.NewDependencies(
+		validator.New(),
+		mockDBPool,
+	)
+	defer deps.CleanupDependencies()
+	// define mock DB expectations
+	rows := mockDBPool.NewRows([]string{"id", "uuid", "created_at", "name", "price"}).
+		AddRow(
+			1,
+			"550e8400-e29b-41d4-a716-446655440000",
+			time.Date(2021, time.January, 1, 0, 0, 0, 0, time.UTC),
+			"pi",
+			float32(3.14),
+		)
+	mockDBPool.ExpectQuery("SELECT (.+) FROM item WHERE id = (.+)").
+		WithArgs(1).
+		WillReturnRows(rows)
+	// setup router
+	r := gin.Default()
+	r.GET("/api/items/:id", routes.HandleGetItem(deps))
+	// exec request
+	w := performRequest(r, "GET", "/api/items/1")
+	// assert response code
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, w.Code)
+	}
+	// assert full response body
+	expected := `{"data":{"id":1,"uuid":"550e8400-e29b-41d4-a716-446655440000","created_at":"2021-01-01T00:00:00Z","name":"pi","price":3.14},"meta":{}}`
+	if w.Body.String() != expected {
+		t.Errorf("Expected %s, but got %s", expected, w.Body.String())
+	}
+	// assert db expectations were met
+	if err := mockDBPool.ExpectationsWereMet(); err != nil {
+		t.Errorf("There were unfulfilled DB expectations: %s", err)
+	}
+}
